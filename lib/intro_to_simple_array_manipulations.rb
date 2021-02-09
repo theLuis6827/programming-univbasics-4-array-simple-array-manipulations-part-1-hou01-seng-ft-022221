cf16e5be0458f963038be632e7a22480c0ee3499
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 07:40:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,8 +10,7 @@ def using_pop (continents = ["Africa", "Asia", "Europe", "North America", "South
   continents.pop
 end
 
-def pop_with_args
-  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+def pop_with_args (dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"])
   small_dogs = dog_breeds.pop(2)
 end
 
@@ -19,7 +18,6 @@ def using_shift (my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Fra
   im_so_over_this_city = my_favorite_cities.shift
 end
 
-def shift_with_args
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+def shift_with_args (ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"])
   brands_removed = ice_cream_brands.shift(2)
 end
