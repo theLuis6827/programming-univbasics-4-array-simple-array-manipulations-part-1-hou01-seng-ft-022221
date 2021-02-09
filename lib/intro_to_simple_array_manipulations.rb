fc17c05a3d2b487f65debcb336a7c49c5dd72a61
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 07:04:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,5 @@
-colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-next_color = ["violet"]
-colors_in_the_rainbow.push(next_color)
+def using_push
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = ["violet"]
+  colors_in_the_rainbow.push(next_color)
+end
