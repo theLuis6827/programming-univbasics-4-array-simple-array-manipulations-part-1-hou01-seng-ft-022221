947033fbf76a0064c52e4311fc158cbcc631426c
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 07:13:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,8 @@ def using_push (colors_in_the_rainbow = ["red", "orange", "yellow", "green", "bl
   colors_in_the_rainbow.push(next_color)
 end
 
-def using_unshift (bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"], )
+def using_unshift (bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"], new_neighborhood = "Staten Island")
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+
