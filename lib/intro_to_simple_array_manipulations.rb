7733c66d95e26051e2feb1ecc8615a2b369e40d5
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 07:14:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,4 +6,4 @@ def using_unshift (bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
-
+def using_pop (continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"], )
